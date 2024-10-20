0ba9dde8b5009d53710532f6e8751ea2eea96821
tjhv10 <achiyabennatan@gmail.com>
2024-10-20 12:34:57 +0300

    cleand unnessecry code

--- tiktokScript.py
+++ tiktokScript.py
@@ -10,75 +10,6 @@ from comments import israel_support_comments
 
 # Global variables
 tiktok_accounts = []
-adb_reset_event = threading.Event()
-action_lock = threading.Lock()
-
-def acquire_action_lock():
-    """
-    Acquire the action lock.
-    """
-    action_lock.acquire()
-
-def release_action_lock():
-    """
-    Release the action lock.
-    """
-    action_lock.release()
-
-
-def get_connected_devices():
-    """
-    Get the list of all connected devices using ADB.
-
-    Returns:
-    list: A list of device IPs or IDs connected via ADB.
-    """
-    result = subprocess.run(['adb', 'devices'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8').strip().splitlines()
-
-    devices = []
-    for line in output[1:]:  # Skip the first line ('List of devices attached')
-        if 'device' in line:  # Make sure it's a connected device
-            device_id = line.split()[0]  # Extract the device ID (first column)
-            devices.append(device_id)
-    
-    return devices
-
-
-device_ips = get_connected_devices()
-
-
-def connect_to_devices():
-    print("Starting connect_to_devices function")
-    for ip in device_ips:
-        result = os.system(f"adb connect {ip}")
-        if result == 0:
-            print(f"Connected to {ip}")
-        else:
-            print(f"Failed to connect to {ip}")
-    print("Finished connect_to_devices function")
-
-
-def restart_adb_periodically(interval=600):
-    while True:
-        time.sleep(interval)
-        print("Setting ADB reset event to pause actions...")
-        adb_reset_event.set()  # Set the event to signal that ADB is resetting
-        print("starting to restart")
-        with action_lock:  # Prevent actions during ADB restart
-            print("Restarting ADB server...")
-            os.system("adb kill-server")
-            time.sleep(2)
-            os.system("adb start-server")
-            print("ADB server restarted.")
-            time.sleep(1)
-            connect_to_devices()
-            time.sleep(2)
-
-        print("Clearing ADB reset event...")
-        adb_reset_event.clear()  # Clear the event to resume actions
-        print("Resuming after ADB reset.")
-
 
 def take_screenshot(d, filename='screenshot_tik.png'):
     """
@@ -98,9 +29,6 @@ def find_best_and_second_best_match(image_path, users_template_path):
     """
     time.sleep(2)
     print("Starting find_best_and_second_best_match function")
-    if adb_reset_event.is_set():
-            print("Paused2 due to ADB reset event.")
-            time.sleep(10)
     img = cv2.imread(image_path)
     template = cv2.imread(users_template_path)
 
@@ -153,58 +81,43 @@ def search(d, text):
     """
     Searches for a specific user on TikTok by simulating clicks and typing.
     """
-    if adb_reset_event.is_set():
-        print("Paused4 due to ADB reset event.")
-        time.sleep(10)
     screen_width = d.info['displayWidth']
     screen_height = d.info['displayHeight']
     
     x = screen_width * (650 / 720)
     y = screen_height * (100 / 1560)
 
-    acquire_action_lock()
-    try:
-        d.click(x, y)  # Click on the search bar
-        time.sleep(2)
+    
+    d.click(x, y)  # Click on the search bar
+    time.sleep(2)
 
-        for char in text:
-            d.send_keys(char, clear=False)
-            time.sleep(random.uniform(0.1, 0.3))
-        time.sleep(2)
-        d.press(66)  # Press the search button
-        time.sleep(5)
+    for char in text:
+        d.send_keys(char, clear=False)
+        time.sleep(random.uniform(0.1, 0.3))
+    time.sleep(2)
+    d.press(66)  # Press the search button
+    time.sleep(5)
 
-        tap_users(d)  # Click to go to users
-        time.sleep(3)
+    tap_users(d)  # Click to go to users
+    time.sleep(3)
 
-        d.click(700, 300)  # Click to go into the first result
-        time.sleep(3)
-    finally:
-        release_action_lock()
+    d.click(700, 300)  # Click to go into the first result
+    time.sleep(3)
+    
         
 
 def tap_like_button(d, like_button_template_path="icons/tiktok_icons/like.png"):
     print("Starting tap_like_button function")
-    
-    if adb_reset_event.is_set():
-        print("Paused2 due to ADB reset event.")
-        time.sleep(13)
-
-    acquire_action_lock()
-    try:
-        screenshot_path = take_screenshot(d)
-        best_match = find_best_and_second_best_match(screenshot_path, like_button_template_path)
-        print(best_match)
-        if best_match:
-            best_coordinates = best_match[0]
-            print(f"Like button found at {best_coordinates}, tapping...")
-            d.click(int(best_coordinates[0]), int(best_coordinates[1]))
-            print(f"Tapped best match at {best_coordinates}.")
-        else:
-            print("Like button not found on the screen.")
-    finally:
-        release_action_lock()
-
+    screenshot_path = take_screenshot(d)
+    best_match = find_best_and_second_best_match(screenshot_path, like_button_template_path)
+    print(best_match)
+    if best_match:
+        best_coordinates = best_match[0]
+        print(f"Like button found at {best_coordinates}, tapping...")
+        d.click(int(best_coordinates[0]), int(best_coordinates[1]))
+        print(f"Tapped best match at {best_coordinates}.")
+    else:
+        print("Like button not found on the screen.")
     print("Finished tap_like_button function")
 
 # def comment_text(d, text):
@@ -237,30 +150,23 @@ def scroll_random_number(d):
     """
     screen_width = d.info['displayWidth']
     screen_height = d.info['displayHeight']
-    if adb_reset_event.is_set():
-        print("Paused7 due to ADB reset event.")
-        time.sleep(10)
-    acquire_action_lock()  # Acquire the lock
-    try:
-        if d(scrollable=True).exists:
-            print("Found a scrollable view! Swiping down...")
-            num_swipes = random.randint(3, 6)
-            print(f"Number of swipes: {num_swipes}")
+    
+    if d(scrollable=True).exists:
+        print("Found a scrollable view! Swiping down...")
+        num_swipes = random.randint(3, 6)
+        print(f"Number of swipes: {num_swipes}")
 
-            for i in range(num_swipes):
-                x_start = screen_width * (500 / 720)
-                y_start = screen_height * (1200 / 1560)
-                x_end = screen_width * (500 / 720)
-                y_end = screen_height * (300 / 1560)
-                d.swipe(x_start, y_start, x_end, y_end, duration=0.05)
-                random_time = random.randint(2, 15)
-                time.sleep(random_time)
-                print(f"Swiped down {i + 1} time(s).")
+        for i in range(num_swipes):
+            x_start = screen_width * (500 / 720)
+            y_start = screen_height * (1200 / 1560)
+            x_end = screen_width * (500 / 720)
+            y_end = screen_height * (300 / 1560)
+            d.swipe(x_start, y_start, x_end, y_end, duration=0.05)
+            random_time = random.randint(2, 15)
+            time.sleep(random_time)
+            print(f"Swiped down {i + 1} time(s).")
         else:
             print("No scrollable view found!")
-    finally:
-        print("relesed6")
-        release_action_lock()  # Ensure the lock is released
 
 
 
@@ -275,10 +181,7 @@ def scroll_and_like(d):
     screen_height = d.info['displayHeight']
 
     for i in range(1):
-        if adb_reset_event.is_set():
-            print("Paused8 due to ADB reset event.")
-            time.sleep(10)
-        acquire_action_lock()  # Acquire the lock
+        
         if d(scrollable=True).exists:
             x_start = screen_width * (500 / 720)
             y_start = screen_height * (1200 / 1560)
@@ -290,10 +193,8 @@ def scroll_and_like(d):
             print(f"Swiped down {i + 1} time(s).")
         else:
             print("No scrollable view found!")
-        release_action_lock()
         if random.choice([1, 2, 3, 4, 5]) < 5:
             tap_like_button(d)
-    acquire_action_lock()
     d.press("back")
     d.press("back")
     time.sleep(2)
@@ -302,34 +203,23 @@ def scroll_and_like(d):
     d.press("back")
     time.sleep(4)
     d.press("back")
-    release_action_lock()
 
 def like_the_page(d, page):
     """
     Likes the specified page and comments on a post.
     """
     search(d, page)
-    acquire_action_lock()
     d.click(120, 1300)
     time.sleep(2)
-    release_action_lock()
     scroll_and_like(d)
 
 def main(d):
     """
     Main function to connect to the device and perform actions on TikTok.
     """
-    connect_to_devices()
-    adb_restart_thread = threading.Thread(target=restart_adb_periodically, daemon=True)
-    adb_restart_thread.start()
-    time.sleep(1)
     d.app_start("com.zhiliaoapp.musically")  # Open TikTok app
     print("Opened TikTok!")
-    if adb_reset_event.is_set():
-        print("Paused1 due to ADB reset event.")
-        time.sleep(10)
     time.sleep(15)
-    
     if "com.zhiliaoapp.musically" in d.app_list_running():
         print("TikTok is running!")
         scroll_random_number(d)
@@ -338,9 +228,6 @@ def main(d):
         time.sleep(1)
         like_the_page(d,"hananyaNaftali")
         scroll_random_number(d)
-        if adb_reset_event.is_set():
-            print("Paused9 due to ADB reset event.")
-            time.sleep(10)
         time.sleep(10)
         d.app_stop("com.zhiliaoapp.musically")
         time.sleep(4)
--- twitterScript.py
+++ twitterScript.py
@@ -1,5 +1,3 @@
-import os
-import subprocess
 import threading
 import numpy as np
 import uiautomator2 as u2
@@ -28,116 +26,14 @@ twitter_handles = [
     "@DannyNis"
 ]
 
-adb_reset_event = threading.Event()
-action_lock = threading.Lock()
-function_work = threading.Event()
-# Global variable to track lock status
-lock_acquired = False
-
-def acquire_action_lock():
-    global lock_acquired
-    print(f"Thread {threading.current_thread().name} attempting to acquire action lock.")
-    action_lock.acquire()
-    lock_acquired = True
-    print(f"Thread {threading.current_thread().name} acquired action lock.")
-
-def release_action_lock():
-    global lock_acquired
-    action_lock.release()
-    lock_acquired = False
-    print(f"Thread {threading.current_thread().name} released action lock.")
-
-import threading
-import time
-import subprocess
-import os
-
-# Example of device IPs and their corresponding ports
-device_config = {
-    "10.100.102.170": 5037,  # Device 1
-    "10.100.102.171": 5038,  # Device 2
-    # Add more device IPs and ports here
-}
-
-# def get_connected_devices():
-#     """
-#     Get the list of all connected devices using ADB.
-
-#     Returns:
-#     list: A list of device IPs or IDs connected via ADB.
-#     """
-#     result = ''
-#     result += subprocess.run(['adb','-P 5038', 'devices'], stdout=subprocess.PIPE)
-#     output = result.stdout.decode('utf-8').strip().splitlines()
-#     print(output)
-#     devices = []
-#     for line in output[1:]:  # Skip the first line ('List of devices attached')
-#         if 'device' in line:  # Make sure it's a connected device
-#             device_id = line.split()[0]  # Extract the device ID (first column)
-#             devices.append(device_id)
-    
-#     return devices
-
-def connect_to_devices():
-    print(f"{threading.current_thread().name}: Starting connect_to_devices function")
-    for ip, port in device_config.items():
-        # Connect to the device on its specific port
-        result = os.system(f"adb -P {port} connect {ip}")
-        if result == 0:
-            print(f"{threading.current_thread().name}: Connected to {ip} on port {port}")
-        else:
-            print(f"{threading.current_thread().name}: Failed to connect to {ip} on port {port}")
-    print(f"{threading.current_thread().name}: Finished connect_to_devices function")
-
-
-def restart_adb_periodically(port,interval=30):
-    while True:
-        time.sleep(interval)
-        # Assuming function_work is a threading.Event() that's being checked somewhere
-        if function_work.is_set():
-            print(f"{threading.current_thread().name}: waiting")
-            while function_work.is_set():
-                print("Sleeping...")
-                time.sleep(0.1)
-            print(f"{threading.current_thread().name}: done waiting")
-        print(f"{threading.current_thread().name}: Setting ADB reset event to pause actions...")
-        adb_reset_event.set()  # Set the event to signal that ADB is resetting
-        acquire_action_lock()  # Lock the action only for the ADB reset thread.
-        try:
-            print(f"{threading.current_thread().name} on {port}: Restarting ADB server...")
-            os.system("adb -P "+str(port)+" kill-server")
-            time.sleep(2)
-            os.system("adb -P "+str(port)+" start-server")
-            print(f"{threading.current_thread().name}: ADB server restarted.")
-            time.sleep(1)
-            connect_to_devices()  # Reconnect to devices after restarting ADB
-            time.sleep(2)
-        finally:
-            release_action_lock()
-
-        print(f"{threading.current_thread().name}: Clearing ADB reset event...")
-        adb_reset_event.clear()  # Clear the event to resume actions
-        print(f"{threading.current_thread().name}: Resuming after ADB reset.")
-
-
-
 
 def tap_like_button(d, like_button_template_path="icons/twitter_icons/like.png"):
     print(f"{threading.current_thread().name}: Starting tap_like_button function")
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused2 due to ADB reset event.")
-        time.sleep(13)
+    
     screenshot_path = take_screenshot(d,threading.current_thread().name)
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused2 due to ADB reset event.")
-        time.sleep(13)
-    best_match, second_best_match = find_best_and_second_best_match(screenshot_path, like_button_template_path)
-    time.sleep(1)
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused2 due to ADB reset event.")
-        time.sleep(13)
-    time.sleep(1)    
-    function_work.set()
+    
+    best_match, second_best_match = find_best_and_second_best_match(screenshot_path, like_button_template_path)    
+    
     if best_match:
         best_coordinates = best_match
         print(f"Like button found at {best_coordinates}, tapping...")
@@ -154,12 +50,11 @@ def tap_like_button(d, like_button_template_path="icons/twitter_icons/like.png")
             print(f"Tapped best match at {best_coordinates}.")
     else:
         print(f"{threading.current_thread().name}: Like button not found on the screen.")
-    function_work.clear()
+    
     print(f"{threading.current_thread().name}: Finished tap_like_button function")
 
 def find_best_and_second_best_match(image_path, template_path):
     print(f"{threading.current_thread().name}: Finding best and second-best match...")
-    function_work.set()
     img = cv2.imread(image_path)
     template = cv2.imread(template_path)
 
@@ -177,47 +72,31 @@ def find_best_and_second_best_match(image_path, template_path):
         matches.remove(best_match)
         second_best_match = max(matches, key=lambda coord: result[coord[1], coord[0]]) if matches else None
         print(f"Best match found at {best_match}, second-best match found at {second_best_match}.")
-        function_work.clear()
+        
         return best_match, second_best_match
     else:
         print(f"{threading.current_thread().name}: No matches found.")
-        function_work.clear()
+        
         return None, None
     
 
 def comment_text(d, text, comment_template_path="icons/twitter_icons/comment.png"):
     print(f"{threading.current_thread().name}: Starting comment_text function")
-    
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused3 due to ADB reset event.")
-        time.sleep(13)
+
     
     screenshot_path = take_screenshot(d,threading.current_thread().name)
     best_match, _ = find_best_and_second_best_match(screenshot_path, comment_template_path)
-    time.sleep(1)
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused10 due to ADB reset event.")
-        time.sleep(13)
-    time.sleep(1)
-    function_work.set()
+    
     if best_match:
         d.click(int(best_match[0]), int(best_match[1]))  # Unpack directly
         time.sleep(2)
-        if adb_reset_event.is_set():
-            print(f"{threading.current_thread().name}: Paused30 due to ADB reset event.")
-            time.sleep(13)
         for char in text:
             d.send_keys(char, clear=False)
-            time.sleep(random.uniform(0.005, 0.01))
-        if adb_reset_event.is_set():
-            print(f"{threading.current_thread().name}: Paused30 due to ADB reset event.")
-            time.sleep(13)    
+            time.sleep(random.uniform(0.005, 0.01)) 
         time.sleep(1)
         d.click(600, 125)  # Click the post button
     else:
         print(f"{threading.current_thread().name}: Comment icon not found on the screen.")
-    
-    function_work.clear()
     print(f"{threading.current_thread().name}: Finished comment_text function")
 
 
@@ -225,10 +104,6 @@ def scroll_like_and_comment(d):
     print(f"{threading.current_thread().name}: Starting scroll_like_and_comment function")
     actions = ['like', 'comment', 'both', 'none']
     for _ in range(30):
-        if adb_reset_event.is_set():
-            print(f"{threading.current_thread().name}: Paused8 due to ADB reset event.")
-            time.sleep(13)
-        function_work.set()
         if d(scrollable=True).exists:
             start_x = random.randint(400, 600)
             start_y = random.randint(900, 1200)
@@ -237,12 +112,11 @@ def scroll_like_and_comment(d):
             d.swipe(start_x, start_y, start_x, end_y, duration=swipe_duration)
             print(f"Scrolled from ({start_x}, {start_y}) to ({start_x}, {end_y}) in {swipe_duration:.2f} seconds.")
         else:
-            print(f"{threading.current_thread().name}: No scrollable view found!")
+            print(f"{threading.current_thread().name}: No scrollable view foun!")
         time.sleep(random.uniform(2, 14))
         action = random.choice(actions)
         print(f"Action chosen: {action}")
         text = random.choice(israel_support_comments)
-        function_work.clear()
         if action == 'like':
             tap_like_button(d)
             print(f"{threading.current_thread().name}: Liked the post.")
@@ -257,29 +131,15 @@ def scroll_like_and_comment(d):
             time.sleep(2)
             comment_text(d, text)
             print(f"Commented: {text}")
-        else:
-            if adb_reset_event.is_set():
-                print(f"{threading.current_thread().name}: Paused6 due to ADB reset event.")
-                time.sleep(13)
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused6 due to ADB reset event.")
-        time.sleep(13)
-    function_work.set()
     d.press("back")
     d.press("back")
-    function_work.clear()
     print(f"{threading.current_thread().name}: Finished scroll_like_and_comment function")
 
 
 def take_screenshot(d,thread):
-    if adb_reset_event.is_set():
-        print(f"{thread}: Paused8 due to ADB reset event.")
-        time.sleep(13)
     filename="Screenshots/"+thread + '-screenshot_twi.png'
     print(f"{thread}: Taking screenshot...")
-    function_work.set()
     d.screenshot(filename)
-    function_work.clear()
     print(f"Screenshot saved as {filename}.")
     return filename
 
@@ -290,10 +150,6 @@ def scroll_random_number(d):
     Parameters:
     d (uiautomator2.Device): The connected device object from uiautomator2.
     """
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused4 due to ADB reset event.")
-        time.sleep(13)
-    function_work.set()
     if d(scrollable=True).exists:
         print(f"{threading.current_thread().name}: Found a scrollable view! Swiping down...")
 
@@ -303,7 +159,6 @@ def scroll_random_number(d):
 
         # Perform the swipe action for the chosen number of times
         for _ in range(num_swipes):
-            function_work.set()
             if d(scrollable=True).exists:
                 start_x = random.randint(400, 600)
                 start_y = random.randint(900, 1200)
@@ -312,7 +167,7 @@ def scroll_random_number(d):
                 d.swipe(start_x, start_y, start_x, end_y, duration=swipe_duration)
                 print(f"Scrolled from ({start_x}, {start_y}) to ({start_x}, {end_y}) in {swipe_duration:.2f} seconds.")
             else:
-                print(f"{threading.current_thread().name}: No scrollable view found!")
+                print(f"{threading.current_thread().name}: No scrollable view founddd!")
             
             time.sleep(random.randint(2, 10))
         time.sleep(3)
@@ -321,8 +176,7 @@ def scroll_random_number(d):
         print(f"{threading.current_thread().name}: Swipped up!")
         time.sleep(3)
     else:
-        print(f"{threading.current_thread().name}: No scrollable view found!")
-    function_work.clear()
+        print(f"{threading.current_thread().name}: No scrollable view foundd!")
 def search_and_go_to_page(d, text):
     """
     Searches for the specified text in Twitter and navigates to the desired page.
@@ -331,10 +185,6 @@ def search_and_go_to_page(d, text):
     d (uiautomator2.Device): The connected device object from uiautomator2.
     text (str): The text to search for.
     """
-    if adb_reset_event.is_set():
-        print(f"{threading.current_thread().name}: Paused5 due to ADB reset event.")
-        time.sleep(13)
-    function_work.set()
     # Perform the search
     d.click(180, 1500)
     print(f"{threading.current_thread().name}: Clicked on the search button.")
@@ -343,36 +193,27 @@ def search_and_go_to_page(d, text):
     # Click on the search input field
     d.click(360, 140)
     time.sleep(5)
-    print("hi")
     # Type each character of the search term with a random delay to simulate human typing
     for char in text:
-        print("hix")
         d.send_keys(char, clear=False)
         time.sleep(random.uniform(0.1, 0.3))  # Random delay between 0.1 and 0.3 seconds
     time.sleep(2)
     print(f"Typed '{text}' in the search bar naturally.")
     d.click(350,250)  # Press Enter (key code 66) after typing the search text
     print(f"{threading.current_thread().name}: Got into the page!")
-    function_work.clear()
     time.sleep(5)
 
 
 
-def main(d,port):
+def main(d):
     """
     The main function connects to the Android device and performs various Twitter actions.
     """
-    # connect_to_devices()
-    adb_restart_thread = threading.Thread(target=restart_adb_periodically, args=(port), daemon=True)
-    adb_restart_thread.start()
-    time.sleep(2)
-    function_work.set()
     # Start the Twitter app
     d.app_start("com.twitter.android")
     print(f"{threading.current_thread().name}: Opened Twitter!")
     time.sleep(7)  # Wait for Twitter to fully load
     d.click(75,1500) # Go to home
-    function_work.clear()
     for _ in range(random.randint(4,10)):
         scroll_random_number(d)
         time.sleep(4)
@@ -384,22 +225,14 @@ def main(d,port):
         time.sleep(2)
         # Perform scrolling and liking of tweets
         scroll_like_and_comment(d)
-        if adb_reset_event.is_set():
-            print(f"{threading.current_thread().name}: Paused11 due to ADB reset event.")
-            time.sleep(13)
-        function_work.set()
         d.click(75,1500) # Go to home
         time.sleep(4)
-        function_work.clear()
         for _ in range(random.randint(4,10)):
             scroll_random_number(d)
             time.sleep(4)
             tap_like_button(d)
             time.sleep(2)
         time.sleep(5)
-    if adb_reset_event.is_set():
-            print(f"{threading.current_thread().name}: Paused11 due to ADB reset event.")
-            time.sleep(13)
     d.app_stop("com.twitter.android")
     time.sleep(4)
 
